1c6069b11c99725f8ea6f0dbe1d9e3b315c3ce55
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-12-24 03:11:22 +0530

    signup is almost setupped

--- server/src/services/verify.send.js
+++ server/src/services/verify.send.js
@@ -1,32 +1,37 @@
 const { PrismaClient } = require("@prisma/client");
 const Prisma = new PrismaClient();
 
-async function verifyOtpSignup(email , otp_id , otp){
-    try {
-        const user = await Prisma.user.findUnique({
-            where:{
-                email:email
-            }
-        })
-        if(user.verify==false){
-        const data = await Prisma.otp.findUnique({
-            where:{
-                id:otp_id
-            }
-        })
-        if(data.otp === otp && data.expirydate<Date.now()){
-           return true
+async function verifyOtpSignup(email, otp_id, otp) {
+  try {
+    const user = await Prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    const otp_user = await Prisma.otp.findUnique({
+      where: {
+        id: otp_id,
+      },
+    });
+
+    if (user) {
+      if (otp_user) {
+        if (user.verify == false) {
+          if (otp_user.otp === otp) {
+            return true;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
         }
-        else{
-           return  false
-        }
+      }
+    } else {
+      return false;
     }
-    else{
-        return 'user already verified'
-    }
-    }catch(err){
-        console.log(err)
-    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-module.exports = verifyOtpSignup
+module.exports = verifyOtpSignup;
--- server/src/signup/services/verifyToken.controller.js
+++ server/src/signup/services/verifyToken.controller.js
@@ -1,13 +1,31 @@
-const verifyOtpSignup = require('../../services/verify.send')
+const verifyOtpSignup = require("../../services/verify.send");
 const wrapper = require("structured-json-response");
+const { PrismaClient } = require("@prisma/client");
+const Prisma = new PrismaClient();
 
-
-
-async function VerifyOtp(req,res){
-    const {email , otp_id , otp} = req.body
-   const result= verifyOtpSignup(email,otp_id,otp)
-   if(result){
-      res.status(200).json(wrapper.ok('user get verified'))
-   }
+async function VerifyOtp(req, res) {
+  const { email, otp_id, otp } = req.body;
+  const result = await verifyOtpSignup(email, otp_id, otp);
+  console.log(result)
+  if (result==true) {
+    try {
+      await Prisma.user.update({
+        where: {
+          email: email,
+        },
+        data: {
+          verify: true,
+        },
+      });
+    } catch (err) {
+      if (err) {
+        res.status(400).json(wrapper.ok("some error ocurred"));
+      } else {
+        res.status(200).json(wrapper.ok("user get verified"));
+      }
+    }
+  }else{
+   res.status(500).json(wrapper.failured('otp id dont matched'))
+  }
 }
-module.exports = {VerifyOtp}
+module.exports = { VerifyOtp };
