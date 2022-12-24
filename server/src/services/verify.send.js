5ce509c28319da8f94f23fc1852bb944af31f4e2
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-12-24 06:19:24 +0530

    check commit

--- server/src/services/verify.send.js
+++ server/src/services/verify.send.js
@@ -20,7 +20,7 @@ async function verifyOtpSignup(email, otp_id, otp) {
           if (otp_user.otp === otp && otp.expirydate<Date.now()) {
             return true;
           } else {
-            return 'expired';
+            return 'otp expired';
           }
         } else {
           return false;
